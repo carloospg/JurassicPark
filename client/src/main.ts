d51e4d142eaeae1aa756baeb70eddfb96ef6c1eb
carloospg <carlosperezgosalvez@gmail.com>
2026-03-06 00:27:15 +0100

    feat: añado el logout

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,47 @@
-import './style.css'
+import './style.css';
 
-console.log("Sistema de Jurassic Park inicializado correctamente.");
+const panelBotones = document.getElementById('panel-botones') as HTMLDivElement;
+
+const token = localStorage.getItem('token_jurassic');
+const userString = localStorage.getItem('user_jurassic');
+
+const renderizarInterfaz = () => {
+    if (!panelBotones) return;
+
+    if (token && userString) {
+        const user = JSON.parse(userString);
+
+        panelBotones.innerHTML = `
+            <div class="alert alert-success mb-3 p-2" role="alert">
+                Bienvenido/a de vuelta, <strong>${user.nick}</strong>
+            </div>
+            
+            <button id="btn-logout" class="btn btn-danger btn-lg fw-bold shadow-sm">
+                <i class="bi bi-box-arrow-right me-2"></i> Cerrar Sesion
+            </button>
+        `;
+
+        const btnLogout = document.getElementById('btn-logout');
+        if (btnLogout) {
+            btnLogout.addEventListener('click', () => {
+                localStorage.removeItem('token_jurassic');
+                localStorage.removeItem('user_jurassic');
+                
+                window.location.reload();
+            });
+        }
+
+    } else {
+        panelBotones.innerHTML = `
+            <a href="/src/login/login.html" class="btn btn-dark btn-lg fw-bold shadow-sm mb-2">
+                <i class="bi bi-box-arrow-in-right me-2"></i> Iniciar Sesion
+            </a>
+            
+            <a href="/src/registro/registro.html" class="btn btn-success btn-lg fw-bold shadow-sm">
+                <i class="bi bi-person-plus-fill me-2"></i> Registrarse
+            </a>
+        `;
+    }
+};
+
+renderizarInterfaz();
