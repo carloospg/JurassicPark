b910239d63293a48dec79d443d9b15bf0ddedac2
carloospg <carlosperezgosalvez@gmail.com>
2026-03-11 16:28:04 +0100

    feat: añado el crud y el navbar

---
+++ client/src/constants.ts
@@ -0,0 +1,21 @@
+const CONSTANTS = {
+    API: {
+        BASE_URL: 'http://127.0.0.1:8000/api/',
+        REGISTER_ENDPOINT: 'registro',
+        LOGIN_ENDPOINT: 'login',
+        PROFILE_UPDATE: 'perfil/actualizar',
+        USERS: 'usuarios',
+    },
+    APP: {
+        NAME: 'Jurassic Park',
+        USER_DEFAULT_AVATAR: 'https://via.placeholder.com/90',
+    },
+    ROUTES: {
+        INDEX: '/',
+        PANEL: '/src/panel/panel.html',
+        PROFILE: '/src/perfil/perfil.html',
+        USERS: '/src/usuarios/usuarios.html',
+    }
+};
+
+export default CONSTANTS;
