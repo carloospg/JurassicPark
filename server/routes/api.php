437cc384550ab05e04531d0b337af6f7820784b5
carloospg <carlosperezgosalvez@gmail.com>
2026-03-11 23:11:49 +0100

    feat: añado el CeldaController y las rutas

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CeldaController;
 
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,5 +17,13 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/{id}', [UserController::class, 'getUsuario']);
         Route::put('/{id}/rol', [UserController::class, 'updateRole']);
         Route::delete('/{id}', [UserController::class, 'deleteUser']);
+    });
+
+    Route::prefix('celdas')->group(function () {
+        Route::get('/', [CeldaController::class, 'getCeldas']);
+        Route::get('/{id}', [CeldaController::class, 'getCelda']);
+        Route::post('/', [CeldaController::class, 'createCelda']);
+        Route::post('/{id}', [CeldaController::class, 'updateCelda']);
+        Route::delete('/{id}', [CeldaController::class, 'deleteCelda']);
     });
 });
